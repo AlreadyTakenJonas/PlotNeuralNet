cb82a289001fc1d3552b1fce1244d209bb4cf44b
Jonas Eichhorn <eichhorn.jonas@uni-jena.de>
2022-12-31 13:55:45 +0100

    moved layers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     from setuptools import setup, find_packages
     from pathlib import Path
-    layerFiles = [str(f) for f in (Path(__file__).parent / "layers").glob("*")]
+    layerFiles = [str(f) for f in (Path(__file__).parent /"PlotNeuralNetPy/layers").glob("*")]
 
     setup(
 	packages=find_packages(),
